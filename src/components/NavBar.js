9bf13171dc8fbb8efe97e0e7dbba83bd5ced8dc2
Franco-Ou <65748031+Franco-Ou@users.noreply.github.com>
2021-07-11 22:22:46 -0300

    Añadí NavBar y Boostrap

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,23 +1,30 @@
-import React from 'react';
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import React from "react";
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import "./NavBar.css";
 
 const NavBar = () => {
-    return (        
+  return (
     <>
-        <Navbar fixed="top" bg="dark" variant="dark">
-            <Navbar.Brand href="#home">Xcommerce</Navbar.Brand>
-            <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-            <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-info">Search</Button>
-            </Form>
-        </Navbar>
+      <Navbar fixed="top" bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Xcommerce</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link href="#home">Mujer</Nav.Link>
+          <Nav.Link href="#features">Hombre</Nav.Link>
+          <Nav.Link href="#pricing">Marcas</Nav.Link>
+        </Nav>
+        <Form inline>
+          <div className="search-box-container">
+            <FormControl
+              type="text"
+              placeholder="¿Qué estás buscando?"
+              className="mr-sm-2"
+            ></FormControl>
+            <i className="fa fa-search" id="search-icon"></i>
+          </div>
+        </Form>
+      </Navbar>
     </>
-    )
-}
+  );
+};
 
 export default NavBar;
